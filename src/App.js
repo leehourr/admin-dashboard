206f6ef1491faf8b58d441f735b87b2e7aac9f21
Lee Hour <lhour999@gmail.com>
2023-03-14 04:03:09 +0700

    wrapped app with colormode context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import React from "react";
+import { ColorModeContext, useMode } from "./Theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content "></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
